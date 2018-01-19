91a099468d5b7e6fd0d4e1768547a13e44f36dc7
Brent Dimmig <brentdimmig@gmail.com>
2018-01-18 19:13:58 -0500

    add cool new sections for tag and stats

--- site/resources/views/pages/home.blade.php
+++ site/resources/views/pages/home.blade.php
@@ -23,6 +23,18 @@
         </div>
     </div>
 </section>
+<section class="hero is-info">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns is-vcentered">
+                <div class="column is-4">
+                    <p class="title">Phin Tag</p>
+                    <p class="subtitle">Check this big thing out!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <section id="services" class="section">
     <div class="container has-text-centered">
         <h1 class="title">Services</h1>
@@ -32,6 +44,38 @@
     </div>
     <div class="container">
         @include('pages.partials.services', compact('services'))
+    </div>
+</section>
+<section class="hero is-danger">
+    <div class="hero-body">
+        <div class="container">
+            <div class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Tweets</p>
+                        <p class="title">3,456</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Following</p>
+                        <p class="title">123</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Followers</p>
+                        <p class="title">456K</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Likes</p>
+                        <p class="title">789</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
